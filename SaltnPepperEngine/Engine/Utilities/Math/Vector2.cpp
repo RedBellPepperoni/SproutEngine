f42a8517d8140ae04dea0fd0521c6aed267a82da
RedBellPepper <abhishek.r.dalvi@gmail.com>
2024-12-21 22:36:25 -0500

    #1 : Vector2 Setup
    Setting up a custom Vector class based on the XMFloat2 from DirectXMaths

---
+++ SaltnPepperEngine/Engine/Utilities/Math/Vector2.cpp
@@ -0,0 +1,240 @@
+/************************************************************************|
+|							     Vector2.cpp						     |
+|************************************************************************|
+|                                                                        |
+|========================================================================|
+|				Copyright (c) 2024 - present Abhishek Dalvi.             |
+|																		 |
+|                                                                        |
+|  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       |
+|  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    |
+|  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.|
+|  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  |
+|  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  |
+|  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     |
+|  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                |
+**************************************************************************/
+
+
+#include <Utilities/Math/Vector2.hpp>
+
+
+namespace SaltnPepperEngine::Maths
+{
+	/// Vector 2 Stuff
+
+	inline Vector2& Vector2::operator+=(const Vector2& _vector) noexcept
+	{
+		using namespace DirectX;
+
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR inVector = XMLoadFloat2(&_vector);
+		const XMVECTOR finalVector = XMVectorAdd(thisVector, inVector);
+
+		XMStoreFloat2(this, finalVector);
+		return *this;
+	}
+
+	inline Vector2& Vector2::operator-=(const Vector2& _vector) noexcept
+	{
+		using namespace DirectX;
+
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR inVector = XMLoadFloat2(&_vector);
+		const XMVECTOR finalVector = XMVectorSubtract(thisVector, inVector);
+
+		XMStoreFloat2(this, finalVector);
+		return *this;
+	}
+
+	inline Vector2& Vector2::operator*=(const Vector2& _vector) noexcept
+	{
+		using namespace DirectX;
+
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR inVector = XMLoadFloat2(&_vector);
+		const XMVECTOR finalVector = XMVectorMultiply(thisVector, inVector);
+
+		XMStoreFloat2(this, finalVector);
+		return *this;
+	}
+
+	// Questionable if vector division is gonna be used 
+	// But using an operator overload anyway
+	inline Vector2& Vector2::operator/=(const Vector2& _vector) noexcept
+	{
+		using namespace DirectX;
+
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR inVector = XMLoadFloat2(&_vector);
+		const XMVECTOR finalVector = XMVectorDivide(thisVector, inVector);
+
+		XMStoreFloat2(this, finalVector);
+		return *this;
+	}
+
+	
+
+	inline Vector2& Vector2::operator*=(const float _float) noexcept
+	{
+		using namespace DirectX;
+
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR finalVector = XMVectorScale(thisVector, _float);
+
+		XMStoreFloat2(this, finalVector);
+		return *this;
+	}
+
+	inline Vector2& Vector2::operator/=(const float _float) noexcept
+	{
+		using namespace DirectX;
+
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR finalVector = XMVectorScale(thisVector, 1.0f / _float);
+
+		XMStoreFloat2(this, finalVector);
+		return *this;
+	}
+
+	
+
+
+	
+	inline bool Vector2::operator==(const Vector2& _vector) const noexcept
+	{
+		using namespace DirectX;
+
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR inVector = XMLoadFloat2(&_vector);
+		return XMVector2Equal(thisVector, inVector);
+	}
+
+	inline bool Vector2::operator!=(const Vector2& _vector) const noexcept
+	{
+		using namespace DirectX;
+
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR inVector = XMLoadFloat2(&_vector);
+		return XMVector2NotEqual(thisVector, inVector);
+	}
+
+	Vector2 Vector2::operator-() const noexcept
+	{
+		return Vector2{ -this->x,-this->y };
+	}
+
+	Vector2 Vector2::operator+() const noexcept
+	{
+		return *this;
+	}
+
+	float Vector2::Length() const noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR finalVector = XMVector2LengthSq(thisVector);
+		return XMVectorGetX(finalVector);
+	}
+
+	float  Vector2::LengthSquared() const noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR thisVector = XMLoadFloat2(this);
+		const XMVECTOR finalVector = XMVector2LengthSq(thisVector);
+		return XMVectorGetX(finalVector);
+	}
+
+	float Vector2::Distance(const Vector2& _firstVector, const Vector2& _secondVector) noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR minVector = XMLoadFloat2(&_firstVector);
+		const XMVECTOR maxVector = XMLoadFloat2(&_secondVector);
+		const XMVECTOR vectorSubtract = XMVectorSubtract(maxVector, minVector);
+		const XMVECTOR finalVector = XMVector2Length(vectorSubtract);
+		return XMVectorGetX(finalVector);
+	}
+
+	float Vector2::DistanceSquared(const Vector2& _firstVector, const Vector2& _secondVector) noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR minVector = XMLoadFloat2(&_firstVector);
+		const XMVECTOR maxVector = XMLoadFloat2(&_secondVector);
+		const XMVECTOR vectorSubtract = XMVectorSubtract(maxVector, minVector);
+		const XMVECTOR finalVector = XMVector2LengthSq(vectorSubtract);
+		return XMVectorGetX(finalVector);
+		
+	}
+
+
+
+
+
+	inline Vector2 operator+(const Vector2& _firstVector, const Vector2& _secondVector) noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR vectorOne = XMLoadFloat2(&_firstVector);
+		const XMVECTOR vectorTwo = XMLoadFloat2(&_secondVector);
+		const XMVECTOR addedVector = XMVectorAdd(vectorOne, vectorTwo);
+		Vector2 finalVector;
+		XMStoreFloat2(&finalVector, addedVector);
+		return finalVector;
+	}
+
+	inline Vector2 operator-(const Vector2& _firstVector, const Vector2& _secondVector) noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR vectorOne = XMLoadFloat2(&_firstVector);
+		const XMVECTOR vectorTwo = XMLoadFloat2(&_secondVector);
+		const XMVECTOR subtractedVector = XMVectorSubtract(vectorOne, vectorTwo);
+		Vector2 finalVector;
+		XMStoreFloat2(&finalVector, subtractedVector);
+		return finalVector;
+	}
+
+	inline Vector2 operator*(const Vector2& _firstVector, const Vector2& _secondVector) noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR vectorOne = XMLoadFloat2(&_firstVector);
+		const XMVECTOR vectorTwo = XMLoadFloat2(&_secondVector);
+		const XMVECTOR multipliedVector = XMVectorMultiply(vectorOne, vectorTwo);
+		Vector2 finalVector;
+		XMStoreFloat2(&finalVector, multipliedVector);
+		return finalVector;
+	}
+
+	Vector2 operator*(const Vector2& _firstVector, const float _float) noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR vectorOne = XMLoadFloat2(&_firstVector);
+		const XMVECTOR multipliedVector = XMVectorScale(vectorOne,_float);
+		
+		Vector2 finalVector;
+		XMStoreFloat2(&finalVector, multipliedVector);
+		return finalVector;
+	}
+
+	inline Vector2 operator/(const Vector2& _firstVector, const Vector2& _secondVector) noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR vectorOne = XMLoadFloat2(&_firstVector);
+		const XMVECTOR vectorTwo = XMLoadFloat2(&_secondVector);
+		const XMVECTOR dividedVector = XMVectorDivide(vectorOne, vectorTwo);
+		Vector2 finalVector;
+		XMStoreFloat2(&finalVector, dividedVector);
+		return finalVector;
+	}
+
+	Vector2 operator/(const Vector2& _firstVector, const float _float) noexcept
+	{
+		using namespace DirectX;
+		const XMVECTOR vectorOne = XMLoadFloat2(&_firstVector);
+		const XMVECTOR multipliedVector = XMVectorScale(vectorOne, 1.0f / _float);
+
+		Vector2 finalVector;
+		XMStoreFloat2(&finalVector, multipliedVector);
+		return finalVector;
+	}
+
+	/// !End of Vector 2 Stuff
+}
---
+++ SaltnPepperEngine/Engine/Utilities/Math/Vector2.hpp
@@ -0,0 +1,121 @@
+/************************************************************************|
+|						    Vector2.hpp						     |
+|************************************************************************|
+|                                                                        |
+|========================================================================|
+|				Copyright (c) 2024 - present Abhishek Dalvi.             |
+|																		 |
+|                                                                        |
+|  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       |
+|  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    |
+|  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.|
+|  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  |
+|  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  |
+|  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     |
+|  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                |
+**************************************************************************/
+
+#ifndef VECTOR_2_H
+#define VECTTOR_2_H
+
+
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXCollision.h>
+
+using namespace DirectX;
+
+namespace SaltnPepperEngine
+{
+	namespace Maths
+	{
+
+		struct Vector2Int;
+		
+
+		/// <summary>
+		///  Structure defining a 2 dimentional floating point vector (a Vector 2)
+		/// </summary>
+		struct Vector2 : public XMFLOAT2
+		{
+			
+			static const Vector2 Zero;
+			static const Vector2 One;
+			static const Vector2 UnitX;
+			static const Vector2 UnitY;
+
+
+
+
+			/// Declarations for Vector2
+			Vector2() noexcept : XMFLOAT2(0.0f, 0.0f) {}
+			constexpr explicit Vector2(float _singleValue) noexcept : XMFLOAT2(_singleValue, _singleValue) {}
+			constexpr explicit Vector2(float _xValue, float _yValue) noexcept : XMFLOAT2(_xValue, _yValue) {}
+
+			Vector2(const Vector2&) = default;
+			Vector2(Vector2&&) = default;
+
+			Vector2(const XMFLOAT2& _xmFloat) noexcept 
+			{ 
+				this->x = _xmFloat.x;  
+				this->y = _xmFloat.y;
+			}
+
+			Vector2(const XMVECTORF32& _xmVector32) noexcept
+			{
+				this->x = _xmVector32.f[0];
+				this->y = _xmVector32.f[1];
+			}
+
+			Vector2(FXMVECTOR _xmVector) noexcept
+			{
+				XMStoreFloat2(this, _xmVector);
+			}
+
+			
+			// Arithmatic
+			Vector2& operator+= (const Vector2& _vector) noexcept;
+			Vector2& operator-= (const Vector2& _vector) noexcept;
+			Vector2& operator*= (const Vector2& _vector) noexcept;
+			Vector2& operator/= (const Vector2& _vector) noexcept;
+			
+
+			Vector2& operator*= (const float _float) noexcept;
+			Vector2& operator/= (const float _float) noexcept;
+
+			// Comparisons
+			bool operator== (const Vector2& _vector) const noexcept;
+			bool operator!= (const Vector2& _vector) const noexcept;
+
+			// Negative Value
+			Vector2 operator- () const noexcept;
+			Vector2 operator+ () const noexcept;
+
+			float Length() const noexcept;
+			float LengthSquared() const noexcept;
+
+			static float Distance(const Vector2& _firstVector, const Vector2& _secondVector) noexcept;
+			static float DistanceSquared(const Vector2& _firstVector, const Vector2& _secondVector) noexcept;
+
+
+
+			
+		};
+
+
+		/// Free-Standing Operators for Left Handeded Operations
+
+		Vector2 operator+ (const Vector2& _firstVector, const Vector2& _secondVector) noexcept;
+		Vector2 operator- (const Vector2& _firstVector, const Vector2& _secondVector) noexcept;
+		Vector2 operator* (const Vector2& _firstVector, const Vector2& _secondVector) noexcept;
+		Vector2 operator* (const Vector2& _firstVector, const float _float) noexcept;
+		Vector2 operator/ (const Vector2& _firstVector, const Vector2& _secondVector) noexcept;
+		Vector2 operator/ (const Vector2& _firstVector, const float _float) noexcept;
+
+	}
+}
+
+
+
+#endif //!VECTOR_2_H
+
